672e8e82296486da35bef985bcd68b166cf9eceb
Bernard Mordan <bernardmordan@gmail.com>
2021-09-01 15:45:04 +0100

    updates cy test to pass! prevent video in cypress.json all works locally

--- cypress/integration/add-user.spec.js
+++ cypress/integration/add-user.spec.js
@@ -3,7 +3,7 @@ const { expect } = require("chai")
 describe('Adding Users', () => {
     it('has a modal opened by an add task button', () => {
         cy.visit(`http://0.0.0.0:3001`)
-        cy.get('#modal').should('not.be.visible')
+        cy.get('#modal').should('not.exist')
         cy.get('.user-list button').click()
         cy.get('#modal').contains('Add User')
     })
