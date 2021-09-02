7dbf8a90d8cd8f0df7abccb0f0fb02ad05518492
Bernard Mordan <bernardmordan@gmail.com>
2021-09-03 00:29:47 +0100

    typo in test localost

--- cypress/integration/add-user.spec.js
+++ cypress/integration/add-user.spec.js
@@ -1,6 +1,6 @@
 describe('Adding Users', () => {
     it('has a modal opened by an add task button', () => {
-        cy.visit(`http://localost:3001`)
+        cy.visit(`http://localhost:3001`)
         cy.get('#modal').should('not.exist')
         cy.get('.user-list button').click()
         cy.get('#modal').contains('Add User')
