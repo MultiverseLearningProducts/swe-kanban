74561e14a5bb6cdbdbe75c46ea4727687d851bdf
Bernard Mordan <bernardmordan@gmail.com>
2021-09-23 09:25:55 +0100

    corrects cypress test address:

--- cypress/integration/add-user.spec.js
+++ cypress/integration/add-user.spec.js
@@ -1,6 +1,6 @@
 describe('Adding Users', () => {
     it('has a modal opened by an add task button', () => {
-        cy.visit(`http://localhost:3001`)
+        cy.visit(`http://kanban:3001`)
         cy.get('#modal').should('not.exist')
         cy.get('.user-list button').click()
         cy.get('#modal').contains('Add User')
